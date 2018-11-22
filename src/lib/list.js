7d6c3ece22e8b0593375b892076d134573bfe5be
vsv8 <vsv8@hi.is>
2018-11-22 16:22:03 +0000

    eitthvað fetch

--- src/lib/list.js
+++ src/lib/list.js
@@ -1,10 +1,18 @@
 import { empty } from './helpers';
+import lectures from '../lectures.json';
 
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
   }
 
+  fetchData() {
+    fetch('../lectures.json')
+      .then((lectures) => {
+          return lectures.json();
+      }
+      .then((data) => console.log(data.lectures[0]));
+  }
   load() {
     empty(this.container);
   }
