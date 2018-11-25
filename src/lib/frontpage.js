28297b025a605aa86219501c0546cfe04942f493
vsv8 <vsv8@hi.is>
2018-11-25 00:46:55 +0000

    hálfklárað button

--- src/lib/frontpage.js
+++ src/lib/frontpage.js
@@ -3,6 +3,12 @@ import { empty, el } from './helpers';
 // const API_URL = '/example.json?domain=';
 const LECTURE_URL = '../lectures.json';
 let frontpage;
+const htmlButton = document.querySelector('.button');
+htmlButton.addEventListener('click', hideLectures)
+const cssButton = document.querySelectorAll('.button')[1];
+cssButton.addEventListener('click', hideLectures);
+const jsButton = document.querySelectorAll('.button')[2];
+jsButton.addEventListener('click', hideLectures);
 
 function displayLectures(lectureList) {
   const container = frontpage.querySelector('.lectures__container');
@@ -16,7 +22,6 @@ function displayLectures(lectureList) {
     container.appendChild(colElement);
   });
 }
-
 
 function fetchLectures(){
   fetch(`${LECTURE_URL}`)
@@ -32,6 +37,37 @@ function fetchLectures(){
     .catch((error) => {
       console.error(error);
     });
+}
+
+let clicked = false;
+function hideLectures(e) {
+  if (e.target.contains(htmlButton)) {
+    if (clicked === false) {
+      htmlButton.classList.add('buttons__button__selected');
+      clicked = true;
+    } else {
+      htmlButton.classList.remove('buttons__button__selected');
+      clicked = false;
+    }
+  }
+  if (e.target.contains(cssButton)) {
+    if (clicked === false) {
+      cssButton.classList.add('buttons__button__selected');
+      clicked = true;
+    } else {
+      cssButton.classList.remove('buttons__button__selected');
+      clicked = false;
+    }
+  }
+  if (e.target.contains(jsButton)) {
+    if (clicked === false) {
+      jsButton.classList.add('buttons__button__selected');
+      clicked = true;
+    } else {
+      jsButton.classList.remove('buttons__button__selected');
+      clicked = false;
+    }
+  }
 }
 
 export function load(_frontpage) {
